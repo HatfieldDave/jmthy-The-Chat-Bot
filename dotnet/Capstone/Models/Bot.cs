9f14002d02fbabd09abaee5a23a47af032f96b2f
David Hatfield <davidhatfield@gmjr@gmail.com>
2022-08-08 10:59:05 -0400

    push 3 day 3 DH

--- dotnet/Capstone/Models/Bot.cs
+++ dotnet/Capstone/Models/Bot.cs
@@ -7,5 +7,28 @@ namespace Capstone.Models
 {
     public class Bot
     {
+        public Bot(int keywordId, string keywords, string reponses)
+        {
+            this.KeywordID = keywordId;
+            this.Keywords = keywords;
+            this.Responses = reponses;
+        }
+
+        public int KeywordID { get; set; }
+        public string Keywords { get; set; }
+        public string Responses { get; set; }
+
+        public List<Bot> ReturnBotList()
+        {
+            Bot newBot = new Bot(1, "hello", "hi");
+            Bot helpBot = new Bot(2, "help", "HOw can I Help you");
+
+            List<Bot> botResponses = new List<Bot>();
+            botResponses.Add(newBot);
+            botResponses.Add(helpBot);
+
+            return botResponses;
+
+        }
     }
 }
