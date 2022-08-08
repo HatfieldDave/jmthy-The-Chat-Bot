ece044ddcfb2cf15385d540d62172aa95490675b
Brice Brown <Brice98Brown@gmail.com>
2022-08-08 14:25:58 -0400

    commit

--- vue/src/views/Greeting.vue
+++ vue/src/views/Greeting.vue
@@ -18,6 +18,7 @@
 </template>
 
 <script>
+import chatService from "../services/ChatService";
 import Messaging from '@/components/Messaging.vue';
 export default {
 	name: "greeting",
@@ -30,6 +31,15 @@ export default {
 		}
 	},
 	methods: {
+		sendInput(){
+			chatService
+			.sendInput(this.messages)
+			.then((response) =>{
+				if(response.status == 200){
+					this.messages.push(response.data)
+				}
+			})
+		}
 		
 	},
 	created() {},
