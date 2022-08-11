5f6af47d384ac53294eb17d9424b4710e10c2909
Brice Brown <Brice98Brown@gmail.com>
2022-08-11 14:36:55 -0400

    pushing so merritt can read

---
+++ dotnet/Capstone/Models/Threshold.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Threshold
+    {
+        public double ThresholdNum { get; set; }
+        public int id { get; set; }
+    }
+}
