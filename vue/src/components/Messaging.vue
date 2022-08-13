61853bb77c7ea3088ec7041a5498ccc1ad4b5a04
Brice Brown <Brice98Brown@gmail.com>
2022-08-13 13:45:38 -0400

    pushing so david can have the stuff

--- vue/src/components/Messaging.vue
+++ vue/src/components/Messaging.vue
@@ -1,26 +1,46 @@
 <template>
   <div class="chat-bot">
-    <ul class="messages-area" style="list-style:none">
-      <li class="greeting botMsgBox">
-        Hello, good to see you,
-        {{ this.$store.state.user.nickname }}! How can I help you?
-        <span class="msg_time"><i>insert time here</i></span>
-      </li>
-      <li v-for="message in messages" v-bind:key="message.botResponse" class="allMessages" >
+    <ul class="messages-area" style="list-style: none" ref="messagesContainer">
+      <div class="greetingMsgContainer">
+        <li id="greeting_message">
+          Hello, good to see you,
+          {{ this.$store.state.user.nickname }}! How can I help you?
+        </li>
+        <span class="msg_time"
+          ><i>{{ getTime() }}</i></span
+        >
+      </div>
+
+      <li
+        v-for="message in messages"
+        v-bind:key="message.botResponse"
+        class="allMessages"
+      >
         <div class="userMsgContainer">
-          <span class="message userMsgBox" id="user_message" v-if="message.userText">
-          {{ message.userText }}
+          <span class="msg_time" v-if="message.userText"
+            ><i>{{ getTime() }}</i></span
+          >
+          <span
+            class="message userMsgBox"
+            id="user_message"
+            v-if="message.userText"
+          >
+            {{ message.userText }}
           </span>
-          <span class="msg_time" v-if="message.userText"><i>insert time here</i></span>
         </div>
         <div class="botMsgContainer">
-          <span class="message botMsgBox" id="bot_message" v-if="message.botResponse">
-          {{ message.botResponse }}
-          <a v-bind:href="message.link" v-show="botMessage.link"> Link </a>
-         </span>
-       <span class="msg_time" v-if="message.botResponse"><i>insert time here</i></span>
+          <span
+            class="message botMsgBox"
+            id="bot_message"
+            v-if="message.botResponse"
+          >
+            {{ message.botResponse }}
+            <a v-bind:href="message.link" v-show="botMessage.link"> Link </a>
+          </span>
+          <span class="msg_time" v-if="message.botResponse"
+            ><i>{{ getTime() }}</i></span
+          >
         </div>
-        
       </li>
     </ul>
     <form class="text-box">
@@ -51,10 +71,7 @@ export default {
   data() {
     return {
       userMessage: {
-       
         userText: "",
-        
-        
       },
       botMessage: {
         //ID: 1,
@@ -63,10 +80,15 @@ export default {
       },
       messageSent: false,
       messages: [],
-
     };
   },
   methods: {
+    getTime() {
+      this.time;
+      let current = new Date();
+      let time = current.getHours() + ":" + current.getMinutes();
+      return time;
+    },
     clearUserMessage() {
       this.userMessage = {
         //ID: this.userMessage.ID+=2,
@@ -98,20 +120,28 @@ export default {
         this.clearBotMessage();
       });
     },
+    scrollToEnd: function () {
+      let content = this.$refs.messagesContainer;
+      content.scrollTop = content.scrollHeight;
+    },
+    updated() {
+      // This will be called when the component updates
+      // try toggling a todo
+      this.scrollToEnd();
+    },
   },
 };
 </script>
 
 <style>
 .chat-bot {
- display: flex;
+  display: flex;
   flex-direction: column;
-  flex-grow: 1;  
+  flex-grow: 1;
 }
 .greeting {
-   display: flex;
+  display: flex;
   align-items: flex-end;
-
 }
 .text-box {
   display: flex;
@@ -122,65 +152,85 @@ export default {
   left: 0rem;
   width: auto;
   height: 5rem;
-
 }
 .messages-area {
   flex-direction: column;
-  align-items:stretch;
-  padding-bottom: 1rem;
+  align-items: stretch;
+  padding-bottom: 5rem;
   align-content: space-between;
-  display:flex;
+  display: flex;
 }
-
 
 .text-box input[type="text"] {
   flex: 1;
-  height: 5rem;  
+  height: 5rem;
 }
 
-.text-box input[type="submit"]{
+.text-box input[type="submit"] {
   height: 5rem;
   width: 5rem;
   background-color: #5bc0de;
 }
 
-
 #user_message {
   border-radius: 20px 20px 5px;
   color: white;
-  background-color: #5bc0de;
+  background-color: #8cd3e8;
   padding: 1%;
   border-radius: 0.6rem;
   display: flex;
   list-style: none;
   align-items: stretch;
-
+  max-width: 40%;
+  margin-bottom: 20px;
+  margin-right: 32px;
 }
 #bot_message {
   border-radius: 5px 20px 20px;
   color: white;
-  background-color: #c5dddb;
+  background-color: #499ab2;
   padding: 1%;
   border-radius: 0.6rem;
   display: flex;
   list-style: none;
   align-items: stretch;
+  max-width: 40%;
+  margin-bottom: 20px;
 }
-.userMsgContainer{
-  display:flex;
+#greeting_message {
+  border-radius: 5px 20px 20px;
+  color: white;
+  background-color: #499ab2;
+  padding: 1%;
+  border-radius: 0.6rem;
+  display: flex;
+  list-style: none;
+  align-items: stretch;
+  max-width: 40%;
+  margin-bottom: 20px;
+}
+.userMsgContainer {
+  display: flex;
   justify-content: flex-end;
   align-items: flex-end;
 }
-.botMsgContainer{
+.botMsgContainer {
   display: flex;
   justify-content: flex-start;
-  flex-direction: row;
-  align-items: flex-start;
+  align-items: flex-end;
 }
-.allMessagesLi{
+.greetingMsgContainer {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+}
+.allMessagesLi {
   display: flex;
   flex-direction: row;
   align-self: stretch;
 }
-
+.msg_time {
+  color: grey;
+  font-size: 14px;
+}
 </style>
