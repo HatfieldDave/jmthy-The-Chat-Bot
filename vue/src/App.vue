673535761e2831369dec0fc7403a76b9f92e8e7c
David Hatfield <davidhatfieldjr@gmail.com>
2022-08-16 15:53:28 -0400

    Recent updates

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,70 +1,74 @@
 <template>
-  <div id="app">
-    <!-- If you start to get random styling you don't like, remove container from this div -->
+	<div id="app">
+		<!-- If you start to get random styling you don't like, remove container from this div -->
 
-    <nav class="navbar navbar-dark bg-primary">
-      <div class="container-fluid">
-        <img class="logo" src="@/assets/Untitled_Artwork.png" alt="" />
-        <a class="navbar-brand" href="#">JMTHY</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarColor01"
-          aria-controls="navbarColor01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="navbarColor01" :key="$route.path">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <router-link class="nav-link" v-bind:to="{ name: 'home' }" >
-                <i class="fas fa-home"></i>
-                <!-- This is a font awesome icon -->
-                Home
-              </router-link>
-              <span class="visually-hidden">(current)</span>
-            </li>
-            <li class="nav-item">
-              <router-link
-                class="nav-link"
-                v-bind:to="{ name: 'register' }"
-                v-if="!$store.state.token"
-                >Register</router-link
-              >
-            </li>
-            <li class="nav-item">
-              <router-link
-                class="nav-link"
-                v-bind:to="{ name: 'login' }"
-                v-if="!$store.state.token"
-                >Login
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link
-                class="nav-link"
-                v-bind:to="{ name: 'logout' }"
-                v-if="$store.state.token"
-                >Logout
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link
-                class="nav-link"
-                v-bind:to="{ name: 'Greeting' }"
-                v-if="$store.state.token"
-                >Chat 
-              </router-link>
-              </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <router-view class="body" />
-  </div>
+		<nav class="navbar navbar-dark bg-primary">
+			<div class="container-fluid">
+				<img class="logo" src="@/assets/JMTHY.png" alt="" />
+				<a class="navbar-brand" href="#">JMTHY</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarColor01"
+					aria-controls="navbarColor01"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div
+					class="navbar-collapse collapse"
+					id="navbarColor01"
+					:key="$route.path"
+				>
+					<ul class="navbar-nav me-auto">
+						<li class="nav-item">
+							<router-link class="nav-link" v-bind:to="{ name: 'home' }">
+								<i class="fas fa-home"></i>
+								<!-- This is a font awesome icon -->
+								Home
+							</router-link>
+							<span class="visually-hidden">(current)</span>
+						</li>
+						<li class="nav-item">
+							<router-link
+								class="nav-link"
+								v-bind:to="{ name: 'register' }"
+								v-if="!$store.state.token"
+								>Register</router-link
+							>
+						</li>
+						<li class="nav-item">
+							<router-link
+								class="nav-link"
+								v-bind:to="{ name: 'login' }"
+								v-if="!$store.state.token"
+								>Login
+							</router-link>
+						</li>
+						<li class="nav-item">
+							<router-link
+								class="nav-link"
+								v-bind:to="{ name: 'logout' }"
+								v-if="$store.state.token"
+								>Logout
+							</router-link>
+						</li>
+						<li class="nav-item">
+							<router-link
+								class="nav-link"
+								v-bind:to="{ name: 'Greeting' }"
+								v-if="$store.state.token"
+								>Chat
+							</router-link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<router-view class="body" />
+	</div>
 </template>
 
 <script>
@@ -75,32 +79,29 @@ export default {};
      Any valid CSS or SCSS is allowable here. See https://sass-lang.com/documentation/style-rules for info on SCSS -->
 <style lang="scss">
 #app {
-  height:100vh;
-	display:grid;
-  grid-template-areas: 
-  "header"
-  "body";
-  grid-template-rows: 1fr 10fr;
+	height: 100vh;
+	display: grid;
+	grid-template-areas:
+		"header"
+		"body";
+	grid-template-rows: 1fr 10fr;
 }
 .logo {
-	width: 50px;
+	width: 100px;
 }
-.body{
-  grid-area: body;
-  
- 
+.body {
+	grid-area: body;
 }
-.navbar{
-  grid-area: header;
+.navbar {
+	grid-area: header;
 }
 .navbar-brand {
-  letter-spacing: 0.5rem;
-  font-weight: 900;
+	letter-spacing: 0.5rem;
+	font-weight: 900;
 }
-.nav-item{
-text-align: right
+.nav-item {
+	text-align: right;
 }
-
 
 @import "./styles/colors.scss";
 
